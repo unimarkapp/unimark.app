8a6885220aa84fc39848f2e9dcaf0a80e9ba5728
Roman Slonov <destabiliseus@gmail.com>
2024-03-22 13:17:33 +0800

    fix: path to trpc server

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,4 +1,4 @@
-import type { AppRouter } from "@bookmark/server/src";
+import type { AppRouter } from "@unimark/server/src";
 import { Routing } from "@/pages";
 import { BrowserRouter } from "react-router-dom";
 import {
--- apps/client/src/shared/trpc.ts
+++ apps/client/src/shared/trpc.ts
@@ -1,4 +1,4 @@
-import type { AppRouter } from "@bookmark/server/src";
+import type { AppRouter } from "@unimark/server/src";
 import { createTRPCReact } from "@trpc/react-query";
 
 export const trpc = createTRPCReact<AppRouter>();
