e744a63057568f3aa22a1be59efd39fd138db4fc
Tushhr <tusharmaheshwaritm6@gmail.com>
2024-04-01 20:43:44 +0530

    fix: file import schema

--- apps/client/src/entities/bookmark/ui/bookmark-import-form/types.ts
+++ apps/client/src/entities/bookmark/ui/bookmark-import-form/types.ts
@@ -1,9 +1,14 @@
 import { z } from "zod";
 
-const ACCEPTED_IMPORT_FILE_TYPE = ['html'];
+const ACCEPTED_IMPORT_FILE_TYPE = ['text/html'];
 
 export const importBookmarkSchema = z.object({
-  importFile: z.instanceof(File).refine((file) => { return ACCEPTED_IMPORT_FILE_TYPE.includes(file.type);}, 'File must be a html file'),
+  importFile: z
+    .custom<File>(val => val instanceof File, 'Please upload a file')
+    .refine(
+      file => ACCEPTED_IMPORT_FILE_TYPE.includes(file.type),
+      { message: 'Please choose .html format files only' }
+    ),
   collectionId: z.string().min(1, "Collection is required"),
 });
 
