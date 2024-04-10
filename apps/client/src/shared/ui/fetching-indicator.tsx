83b8738a4d2565ac393899f65d23071febd22d7e
Roman Slonov <destabiliseus@gmail.com>
2024-04-10 10:43:50 +0800

    feat(fetching-indicator): add delay

--- apps/client/src/shared/ui/fetching-indicator.tsx
+++ apps/client/src/shared/ui/fetching-indicator.tsx
@@ -1,5 +1,6 @@
 import { Loader2 } from "lucide-react";
 import { cn } from "../lib";
+import { useSpinDelay } from "spin-delay";
 
 interface Props {
   isFetchingNextPage: boolean;
@@ -7,13 +8,15 @@ interface Props {
 }
 
 export function FetchingIndicator({ isFetchingNextPage, isRefetching }: Props) {
+  const delayedPending = useSpinDelay(isFetchingNextPage || isRefetching, {
+    delay: 100,
+    minDuration: 400,
+  });
   return (
     <div
       className={cn(
         "fixed left-[50%] flex items-center gap-2 -translate-x-[50%] bottom-4 rounded-full bg-background/50 p-2 pr-3 border backdrop-blur-sm transition-transform duration-200 shadow",
-        isFetchingNextPage || isRefetching
-          ? "translate-y-0"
-          : "translate-y-[calc(100%_+_1rem)]"
+        delayedPending ? "translate-y-0" : "translate-y-[calc(100%_+_1rem)]"
       )}
     >
       <Loader2 className="w-5 h-5 animate-spin" />
