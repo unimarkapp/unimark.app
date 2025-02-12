1ef1bcc6392701f7d804d68eb2c4722cad3379f2
Roman Slonov <destabiliseus@gmail.com>
2025-02-12 19:26:12 +0800

    wip

--- drizzle/0010_classy_diamondback.sql
+++ drizzle/0010_classy_diamondback.sql
@@ -1,5 +1,7 @@
 -- Custom SQL migration file, put your code below! --
--- Step 1: Check all users if they don't have organizations, then create one with slug: 'default'
+-- Step 1: Add organization_id column to the tag table (nullable initially)
+ALTER TABLE bookmark ADD COLUMN organization_id text;
+-- Step 2: Check all users if they don't have organizations, then create one with slug: 'default'
 DO $$
 DECLARE
     user_record RECORD;
@@ -20,7 +22,7 @@ BEGIN
     END LOOP;
 END $$;
 
--- Step 3: Check all user's bookmarks if organization_id is empty, then get the default organization of the user (from step 1) and set it for each bookmark
+-- Step 4: Check all user's bookmarks if organization_id is empty, then get the default organization of the user (from step 1) and set it for each bookmark
 DO $$
 DECLARE
     user_record RECORD;
@@ -39,3 +41,6 @@ BEGIN
         WHERE owner_id = user_record.id AND organization_id IS NULL;
     END LOOP;
 END $$;
+
+-- Step 5: Add NOT NULL constraint to the organization_id column
+ALTER TABLE bookmark ALTER COLUMN organization_id SET NOT NULL;
