01ce8dfc81698a1bf96541ea57bcc96e285d89f6
Roman Slonov <destabiliseus@gmail.com>
2025-02-13 10:31:48 +0800

    wip

--- app/(dashboard)/[organizationId]/trash/page.tsx
+++ app/(dashboard)/[organizationId]/trash/page.tsx
@@ -1,16 +1,19 @@
 import { HydrateClient, api } from '@/trpc/server';
 import { BookmarksTrashedList } from '@/widgets/bookmark/bookmarks-trashed-list';
 
-export default async function Tash({
-  searchParams,
-}: {
-  searchParams: Promise<{ query?: string; tags?: string }>;
-}) {
-  const { query = null, tags = null } = await searchParams;
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
+type Params = Promise<{ organizationId: string }>;
+
+export default async function Tash(props: { params: Params; searchParams: SearchParams }) {
+  const { organizationId } = await props.params;
+  const searchParams = await props.searchParams;
+  const query = searchParams.query ?? null;
+  const tags = searchParams.tags ?? null;
 
   await api.bookmark.list.prefetchInfinite({
-    query,
-    tags: tags?.split(',') ?? null,
+    query: Array.isArray(query) ? query.join(',') : query,
+    tags: typeof tags === 'string' ? [tags] : tags,
+    organizationId,
     deleted: true,
   });
 
